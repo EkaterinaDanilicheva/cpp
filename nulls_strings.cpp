26695c2c2a75340c1be196f76fb470a267355f9a
EkaterinaDanilicheva <sigekamig@yandex.ru>
2017-08-11 12:13:49 +0300

    add nulls_strings.cpp

---
+++ nulls_strings.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+ for (int str=1; str<=5; str++) {
+	for(int el=0; el<str; el++) {
+ 		cout << "0";
+	}
+	cout << "\n";
+ }
+ return 0;
+}
